d219d3257f027c438d6f78588369b314ebc7dfe9
Yadhu <yadhukrishnanas@gmail.com>
2023-09-19 20:37:12 +0530

    admin

---
+++ Controllers/AdminController.js
@@ -0,0 +1,4 @@
+import User from "../Models/UserModel.js";
+import Token from "../Models/TokenModel.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
--- Routes/AdminRoute.js
+++ Routes/AdminRoute.js
@@ -1,4 +1,6 @@
 import Express from "express";
- 
+import AdminController from '../Controllers/AdminController' 
 const AdminRout = Express.Router();
+
+AdminRout.post("/login",()=>console.log('hello admin backend'))
 
