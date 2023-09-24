aa97900b06d1312ee2749888b0b8d3ff347c4898
Yadhu <yadhukrishnanas@gmail.com>
2023-09-24 19:09:08 +0530

    lawyer

--- Routes/AdminRoute.js
+++ Routes/AdminRoute.js
@@ -1,12 +1,12 @@
 import Express from "express";
 const AdminRout = Express.Router();
-import AdminController from "../Controllers/AdminController.js"; 
+import AdminController from "../Controllers/AdminController.js"
 
 AdminRout.post("/login",AdminController.login)
 AdminRout.get('/users',AdminController.getUsers)
 AdminRout.get('/lawyers',AdminController.getLawyers)
-AdminRout.put("/managelawyer/:id", AdminController.manageLawyers);
-AdminRout.put("/manageuser/:id", AdminController.manageUsers);
+AdminRout.put("/managelawyer/:id", AdminController.manageLawyers)
+AdminRout.put("/manageuser/:id", AdminController.manageUsers)
 
 
 export default AdminRout;
