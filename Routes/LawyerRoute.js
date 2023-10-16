6d41d03e6b190541926c63dc9791dd556fa4be74
Yadhu <yadhukrishnanas@gmail.com>
2023-10-16 16:52:53 +0530

    lawyer auth

--- Routes/LawyerRoute.js
+++ Routes/LawyerRoute.js
@@ -2,13 +2,14 @@ import Express from "express";
 const LawyerRoute = Express.Router();
 import LawyerController from "../Controllers/LawyerController.js";
 import upload from "../Middleware/Multer.js";
+import { lawyerAuth } from "../Middleware/Auth.js";
 
 LawyerRoute.post("/register", LawyerController.register);
 LawyerRoute.get("/:id/verify/:token", LawyerController.verification);
 LawyerRoute.post("/googleSignup", LawyerController.SignupWithGoogle);
 LawyerRoute.post('/login',LawyerController.login)
-LawyerRoute.get("/lawyerData/:id", LawyerController.lawyerData);
-LawyerRoute.post("/profileEdit/:id", LawyerController.profileEdit);
-LawyerRoute.put("/aboutEdit/:id", LawyerController.aboutEdit);
-LawyerRoute.post("/imgupdate",upload.single("image"), LawyerController.updateImage)
+LawyerRoute.get("/lawyerData/:id",lawyerAuth , LawyerController.lawyerData);
+LawyerRoute.post("/profileEdit/:id",lawyerAuth, LawyerController.profileEdit);
+LawyerRoute.put("/aboutEdit/:id",lawyerAuth, LawyerController.aboutEdit);
+LawyerRoute.post("/imgupdate",lawyerAuth,upload.single("image"), LawyerController.updateImage)
 export default LawyerRoute;
